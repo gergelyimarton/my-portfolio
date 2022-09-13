055c5aeb5f2e6e007c0961d6e33bf6222f0d25f6
Paul Cristian <paul931224@gmail.com>
2022-09-13 23:57:29 +0200

    Parallax works.

--- source-code/portfolio/home.cljs
+++ source-code/portfolio/home.cljs
@@ -1,32 +1,16 @@
 (ns portfolio.home
   (:require
-   ["react-parallax" :default Parallax]
+   ["react-parallax" :refer (Parallax)]
    [reagent.core :as reagent :refer [atom]]))
 
-
-
-(defn parallax []
-  (reagent/create-class {:component-did-mount (fn [this] (let [scene (.getElementById js/document "parallax")]
-                                                           (js/Parallax. scene)))
-                         :reagent-render (fn []
-                                           [:div#parallax
-                                            [:div.background {:data-depth 0.4}]
-                                            [:div {:data-depth 0} "Szöveg"]])}))
-
-
-;;/////////////// home page structure /////////////////////////////////////////
-
-
 (defn view []
-  [:div.base
-   [:> Parallax {:blur 10
-                 :bgImage "/images/clouds.jpg"
-                 :bgImageAlt "the cat"
-                 :strength 200}]
+  [:> Parallax {:bgImage "/images/clouds.jpg"
+                :bgImageAlt "the cat"
+                :strength 1000}
 
-   [:div.home
-    [:div.title "hello friend!"]
-    [:div.subtitle
-     [:div "My name is Marci"]
-     [:div "and this is my portfolio project!"]]
-    [:div {:style {:height "10000px"}}]]])
+    [:div.max-width-container 
+     [:div.title "hello friend!"]
+     [:div.subtitle
+      [:div "My name is Marci"]
+      [:div "and this is my portfolio project!"]]
+     [:div {:style {:height "1000px"}}]]])
