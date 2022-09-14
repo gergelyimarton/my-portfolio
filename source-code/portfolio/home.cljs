a75ea47ed1b29a91a0fb2a0809f82cff17777686
Gergelyi Márton <gergelyimarci@gmail.com>
2022-09-14 19:51:04 +0200

    right card, button done!

--- source-code/portfolio/home.cljs
+++ source-code/portfolio/home.cljs
@@ -3,10 +3,30 @@
    ["react-parallax" :refer (Parallax)]
    [reagent.core :as reagent :refer [atom]]))
 
+
+(defn arrow-back []
+  [:img {:src "/images/arrow_back.svg"
+              :style {
+                      :padding-left "20px"
+                      :height "70px"
+                      :width "70px"}}])
+
+
+
+
+
+
+
+
+
+
+
+;;/////////////// home page architecture /////////////////////////////////////////
+
 (defn view []
   [:> Parallax {:bgImage "/images/clouds.jpg"
                 :bgImageAlt "the cat"
-                :strength 1000}
+                :strength 1100}
 
     [:div.max-width-container
      [:div.title "hello friend!"]
@@ -15,8 +35,9 @@
       [:div "and this is my portfolio project!"]]
      [:div.card-container {:style {:height "1000px"
                                    :padding-top "500px"}}
-      [:div.right-card
-       [:div.button-right "button"]
+      [:div.right-card-row
+       [:div.button-right
+        [arrow-back]]
        [:div.about-me "About me"]
-       [:div.about-me-text
-        [:div.inner-text "text"]]]]]])
+       [:div.card
+        [:div.inner-text "Is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."]]]]]])
