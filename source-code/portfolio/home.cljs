ddc058382904f5303524dc617c00168f97ec1b71
Gergelyi Márton <gergelyimarci@gmail.com>
2022-09-14 20:49:43 +0200

    left card, button done!

--- source-code/portfolio/home.cljs
+++ source-code/portfolio/home.cljs
@@ -11,6 +11,13 @@
                       :height "70px"
                       :width "70px"}}])
 
+(defn arrow-forward []
+  [:img {:src "/images/arrow_forward.svg"
+              :style {
+                      :padding-left "20px"
+                      :height "70px"
+                      :width "70px"}}])
+
 
 
 
@@ -33,11 +40,18 @@
      [:div.subtitle
       [:div "My name is Marci"]
       [:div "and this is my portfolio project!"]]
-     [:div.card-container {:style {:height "1000px"
-                                   :padding-top "500px"}}
+     [:div.card-container
+
       [:div.right-card-row
        [:div.button-right
         [arrow-back]]
        [:div.about-me "About me"]
        [:div.card
-        [:div.inner-text "Is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."]]]]]])
+        [:div.inner-text "Is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."]]]
+
+      [:div.left-card-row
+       [:div.card
+        [:div.inner-text "Is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English."]]
+       [:div.my-skills "My skills"]
+       [:div.button-left
+        [arrow-forward]]]]]])
